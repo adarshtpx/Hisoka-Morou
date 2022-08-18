b4a0e9257c26c182a6c823bec6e0246593a6a244
adarshtpx <90949808+adarshtpx@users.noreply.github.com>
2022-08-18 06:40:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['917306781647']
+global.premium = ['917306781647']
+global.packname = 'Umb Bruh'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'tpx'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
